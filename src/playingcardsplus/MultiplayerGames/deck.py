bd1ff9aa105fd50439ef44b961dfb5112cf1d8ed
TheRealDataMonster <kevinkim1207@gmail.com>
2025-11-04 16:46:08 -0500

    deck testing done

--- src/playingcardsplus/MultiplayerGames/deck.py
+++ src/playingcardsplus/MultiplayerGames/deck.py
@@ -1,137 +1,136 @@
 from playingcardsplus.card import Card, JokerCard
 from playingcardsplus.deck import AbstractDeck
-from playingcardsplus.custom_error import DuplicateCardError, DeckInlclusionError, UnrecognizedCardError, DealerUnassignedError
+from playingcardsplus.custom_error import (
+    DuplicateCardError,
+    DeckInlclusionError,
+    UnrecognizedCardError,
+    DealerUnassignedError
+)
 
 from typing_extensions import List, OrderedDict, Deque, Iterable, Self
 from pydantic import PrivateAttr, NonNegativeInt, model_validator
 
 
 class MultiPlayerDeck(AbstractDeck):
-    # TODO: creating visibility over this can allow Dealer to cheat, let's make sure the condition for this is set? so only cheeating can operate it?
-    _unused: Deque[Card|JokerCard] = PrivateAttr(Deque[Card|JokerCard]()) # Make it LIFO - OrderedDict is by defaulkt but making last=True just in case
-    _board: OrderedDict[Card|JokerCard, bool] = PrivateAttr(OrderedDict[Card|JokerCard, bool]()) # Make it LIFO
-    _trash_pile: Deque[Card|JokerCard] = PrivateAttr(Deque[Card|JokerCard]()) # Make it LIFO
-    _player_hands: OrderedDict[Card|JokerCard, bool] = PrivateAttr(OrderedDict[Card|JokerCard, bool]()) # Just track what cards are distriburted amongst players. This way the dealer doesn't have to track who has what hands
-    _dealer_assigned: bool = False #TODO: is this necessary? I thin keventually this has to be some kind of handshake auth thing btw dealer and deck
+    # TODO: making sure we can track when Dealers or Game cheats?
+    __unused: Deque[Card | JokerCard] = PrivateAttr(
+        Deque[Card | JokerCard]()
+    )  # Make it LIFO
+    __board: OrderedDict[Card | JokerCard, bool] = PrivateAttr(
+        OrderedDict[Card | JokerCard, bool]()
+    )  # Make it LIFO
+    __trash_pile: Deque[Card | JokerCard] = PrivateAttr(
+        Deque[Card | JokerCard]()
+    )  # Make it LIFO
+    __player_hands: OrderedDict[Card | JokerCard, bool] = PrivateAttr(
+        OrderedDict[Card | JokerCard, bool]()
+    )  # Just track what cards are distriburted amongst players. This way the dealer doesn't have to track who has what hands
+    __dealer_assigned: bool = PrivateAttr(default=False)  # TODO: eventually this has to be some kind of handshake auth thing btw dealer and deck
 
-    @model_validator(mode='after')
-    def move_cards_to_unused(self) -> Self:
-        self._unused = Deque[Card|JokerCard](self.shuffled_cards)
+    @model_validator(mode="after")
+    def __move_cards_to_unused(self) -> Self:
+        self.__unused = Deque[Card | JokerCard](self.shuffled_cards)
         return self
 
     @property
-    def unused(self) -> Deque[Card|JokerCard]:
-        return self._unused
+    def unused(self) -> Deque[Card | JokerCard]:
+        return self.__unused
 
     @property
-    def board(self) -> OrderedDict[Card|JokerCard, bool]:
-        return self._board
+    def board(self) -> OrderedDict[Card | JokerCard, bool]:
+        return self.__board
 
     @property
-    def trash_pile(self) -> Deque[Card|JokerCard]:
-        return self._trash_pile
+    def trash_pile(self) -> Deque[Card | JokerCard]:
+        return self.__trash_pile
 
     @property
-    def player_hands(self) -> OrderedDict[Card|JokerCard, bool]:
-        return self._player_hands
+    def player_hands(self) -> OrderedDict[Card | JokerCard, bool]:
+        return self.__player_hands
 
     @property
     def dealer_assigned(self) -> bool:
-        return self._dealer_assigned
+        return self.__dealer_assigned
 
-    @dealer_assigned.setter
-    def _assign_dealer(self):
-        self._dealer_assigned = True
+    def _toggle_assignment(self):
+        self.__dealer_assigned = not self.__dealer_assigned
 
-    @dealer_assigned.setter
-    def _unassign_dealer(self):
-        self._dealer_assigned = False
-
-    # *** Below functions are meant to be used by the Dealer to figure out what cards go from where to where
-
-    def _take_from_unused(self, used_count: NonNegativeInt) -> Deque[Card|JokerCard]: # When updating the Deck, this is the first or second thing that needs to occur
-        if self._dealer_assigned is False:
+    # *** Below functions are meant to be used by the Dealer to manipulate the deck as needed
+    def _take_from_unused(self, used_count: NonNegativeInt) -> Deque[Card | JokerCard]:  # When updating the Deck, this is the first or second thing that needs to occur
+        if self.__dealer_assigned is False:
             raise DealerUnassignedError()
         # TODO: it's uncertain yet whether it needs to handle how it's being used here and match it againt instruction sets or this be done elsewhere
-        used = Deque[Card|JokerCard]()
+        used = Deque[Card | JokerCard]()
         for i in range(used_count):
-            used.append(self._unused.pop())
-            #TODO: might have to use a condition here that checks card for Deck Recognition - ie.does the deck include joker or not?
+            used.append(self.__unused.pop())
+            # TODO: might have to use a condition here that checks card for Deck Recognition - ie.does the deck include joker or not?
         return used
 
-
-    def _replenish_unused(self, replenishers: List[Card|JokerCard]): # This doesn't handle duplicates, IF it happens, then it's a problem with the Deck initiation and potential cheating
-        if self._dealer_assigned is False:
+    def _replenish_unused(self, replenishers: List[Card | JokerCard]):  # This doesn't handle duplicates, IF it happens, then it's a problem with the Deck initiation and potential cheating
+        if self.__dealer_assigned is False:
             raise DealerUnassignedError()
         for card in replenishers[::-1]:
-            self._unused.appendleft(card)
+            self.__unused.appendleft(card)
 
-
-    @board.setter
-    def _remove_from_board(self, removed: OrderedDict[Card|JokerCard, bool]) -> OrderedDict[Card|JokerCard, bool]: #TODO: THIS forces to create some sort of aggregate List, let's see if it's efficient or it's better to list it othersie
-        if self._dealer_assigned is False:
+    def _remove_from_board(self, removed: OrderedDict[Card | JokerCard, bool]) -> OrderedDict[Card | JokerCard, bool]:  # TODO: THIS forces to create some sort of aggregate List, let's see if it's efficient or it's better to list it othersie
+        if self.__dealer_assigned is False:
             raise DealerUnassignedError()
-        removed_cards = OrderedDict[Card|JokerCard, bool]()
+        removed_cards = OrderedDict[Card | JokerCard, bool]()
         for card, remove_status in removed.items():
             if remove_status is True:
-                removed_card = self._board.pop(card , "Not Found")
-                if not removed_card: # Have to remove a card but it's not there?
+                removed_card = self.__board.pop(card, "Not Found")
+                if not removed_card:  # Have to remove a card but it's not there?
                     raise DeckInlclusionError()
-                if removed_card == "Not Found": # Card key is not om the board somehow?
+                if removed_card == "Not Found":  # Card key is not om the board somehow?
                     raise UnrecognizedCardError()
                 else:
                     removed_cards[card] = True
-        return removed_cards # this needs to be like reverse of removed
+        return removed_cards  # this needs to be like reverse of removed
 
-    @board.setter
-    def _add_to_board(self, added: Iterable[Card|JokerCard]):
-        if self._dealer_assigned is False:
+    def _add_to_board(self, added: Iterable[Card | JokerCard]):
+        if self.__dealer_assigned is False:
             raise DealerUnassignedError()
-        for card in added: #
-            if self._board[card] is True: # how can you add it if it's already there?
+        for card in added:  #
+            if self.__board[card] is True:  # how can you add it if it's already there?
                 raise DuplicateCardError()
-            self._board[card] = True
+            self.__board[card] = True
 
-
-    @trash_pile.setter
-    def _burn_trash(self, burn_count: NonNegativeInt) -> Deque[Card|JokerCard]: #
-        if self._dealer_assigned is False:
+    def _burn_trash(self, burn_count: NonNegativeInt) -> Deque[Card | JokerCard]:  #
+        if self.__dealer_assigned is False:
             raise DealerUnassignedError()
-        burnt = Deque[Card|JokerCard]()
+        burnt = Deque[Card | JokerCard]()
         for i in range(burn_count):
-            burnt.append(self._trash_pile.pop())
-            #TODO: might have to use a condition here that checks card for Deck Recognition - ie.does the deck include joker or not?
+            burnt.append(self.__trash_pile.pop())
+            # TODO: might have to use a condition here that checks card for Deck Recognition - ie.does the deck include joker or not?
         return burnt
 
-    @trash_pile.setter
-    def _add_trash(self, trashed: Iterable[Card|JokerCard]):
-        if self._dealer_assigned is False:
+    def _add_trash(self, trashed: Iterable[Card | JokerCard]):
+        if self.__dealer_assigned is False:
             raise DealerUnassignedError()
         for card in trashed:
-            self._trash_pile.append(card)
+            self.__trash_pile.append(card)
 
-
-    @player_hands.setter
-    def _take_from_players(self, removed: OrderedDict[Card|JokerCard, bool]) -> OrderedDict[Card|JokerCard, bool]:
-        if self._dealer_assigned is False:
+    def _take_from_players(self, removed: OrderedDict[Card | JokerCard, bool]) -> OrderedDict[Card | JokerCard, bool]:
+        if self.__dealer_assigned is False:
             raise DealerUnassignedError()
-        taken_cards = OrderedDict[Card|JokerCard, bool]()
+        taken_cards = OrderedDict[Card | JokerCard, bool]()
         for card, remove_status in removed.items():
             if remove_status is True:
-                removed_card = self._player_hands.pop(card , "Not Found")
-                if not removed_card: # Have to remove a card but it's not there?
+                removed_card = self.__player_hands.pop(card, "Not Found")
+                if not removed_card:  # Have to remove a card but it's not there?
                     raise DeckInlclusionError()
-                if removed_card == "Not Found": # Card key is not om the board somehow?
+                if removed_card == "Not Found":  # Card key is not om the board somehow?
                     raise UnrecognizedCardError()
                 else:
                     taken_cards[card] = True
-        return taken_cards # this needs to be like reverse of removed
+        return taken_cards  # this needs to be like reverse of removed
 
-    @player_hands.setter
-    def _give_to_players(self, distributed: Iterable[Card|JokerCard]):
-        if self._dealer_assigned is False:
+    def _give_to_players(self, distributed: Iterable[Card | JokerCard]):
+        if self.__dealer_assigned is False:
             raise DealerUnassignedError()
         for card in distributed:
-            if self._player_hands[card] is True: # how can you add it if it's already there?
+            if (
+                self.__player_hands[card] is True
+            ):  # how can you add it if it's already there?
                 raise DuplicateCardError()
-            self._player_hands[card] = True
+            self.__player_hands[card] = True
--- tests/test_decks.py
+++ tests/test_decks.py
@@ -1,12 +1,11 @@
 from playingcardsplus.MultiplayerGames.deck import MultiPlayerDeck
+from playingcardsplus.custom_error import DealerUnassignedError
 from playingcardsplus.card import Color
 
 from collections import Counter
+from typing_extensions import OrderedDict
+import pytest
 
-CLUBS = "♣"
-DIAMONDS = "♦"
-HEARTS = "♥"
-SPADES = "♠"
 
 __french_deck = {
     ("2", "♣"), ("2", "♦"), ("2", "♥"), ("2", "♠"),
@@ -24,40 +23,36 @@ __french_deck = {
     ("ace", "♣"), ("ace", "♦"), ("ace", "♥"), ("ace", "♠"),
 }
 
-def test_french_multiplayer_deck_creation():
+
+def test_french_multiplayer_creation():
     """
     Test for the following
     1) Creation done well = Same 52 cards as specified above with no duplicates
     2) Distribution makes sense - unused, board, player hands, trash pile add up to cards
     """
-    deck = MultiPlayerDeck(
-        name="French_Multi_Player_Deck_NoJocker",
-        joker_count=0
-    )
+    deck = MultiPlayerDeck(name="French_Multi_Player_Deck_NoJocker", joker_count=0)
     # 1) Same 52 cards, no duplicates
-    assert(len(deck) == 52)
-    assert(Counter(deck.cards) == Counter(__french_deck))
-    assert(Counter(deck.unused) == Counter(__french_deck))
+    assert len(deck) == len(__french_deck)
+    assert Counter(deck.cards) == Counter(__french_deck)
+    assert Counter(deck.unused) == Counter(__french_deck)
 
     # 2) Things add up!
-    print(Counter(deck.unused))
-    assert(Counter(deck.cards) == (
-        Counter(deck.unused) + Counter(deck.trash_pile) +
-        Counter(deck.board) + Counter(deck.player_hands)
-    ))
+    assert Counter(deck.cards) == (
+        Counter(deck.unused)
+        + Counter(deck.trash_pile)
+        + Counter(deck.board)
+        + Counter(deck.player_hands)
+    )
 
-def test_french_multiplayer_deck_creation_with_jokers():
+
+def test_french_multiplayer_creation_with_jokers():
     """
     Test for the following
     1) Creation done well = Same 54 cards as specified above with no duplicates
-    2)
+    2) Distribution makes sense - unused, board, player hands, trash pile add up to cards
     """
-    deck = MultiPlayerDeck(
-        name="French_Multi_Player_Deck_Joker",
-        joker_count=2
-    )
-    # Same 54 cards, no duplicates
-    assert(len(deck)==54)
+    deck = MultiPlayerDeck(name="French_Multi_Player_Deck_Joker", joker_count=2)
+
     __french_with_joker = set()
     __french_with_joker.update(__french_deck)
 
@@ -65,23 +60,59 @@ def test_french_multiplayer_deck_creation_with_jokers():
     while i <= 2:
         for color_member in Color:
             __french_with_joker.add((color_member.value, i))
-            i+=1
+            i += 1
             if i > 2:
                 break
 
-    assert(Counter(deck.cards) == Counter(__french_with_joker))
+    # 1) Same 54 cards, no duplicates
+    assert len(deck) == len(__french_with_joker)
+    assert Counter(deck.cards) == Counter(__french_with_joker)
+    assert Counter(deck.unused) == Counter(__french_with_joker)
 
-# TODO:
-# class TestDeckActions(unittest.TestCase):
-#     # TODO: bunch of functions like Deck taking actions with/without Dealer assigned + Dealer assigned functions do work properly as intended
-#     # TODO: hacking Dealer assignment without Game assignment -> should fail
-#     # TODO: unassigning Dealer without proper validation
-#     pass
-#
+    # 2) Things add up!
+    assert Counter(deck.cards) == (
+        Counter(deck.unused)
+        + Counter(deck.trash_pile)
+        + Counter(deck.board)
+        + Counter(deck.player_hands)
+    )
 
 
 
+@pytest.mark.parametrize("func_name, args, expected_exception, expected_message", [
+    ("_take_from_unused", [5], DealerUnassignedError, "Dealer has not been assigned to this Deck"),
+    ("_replenish_unused", [[]], DealerUnassignedError, "Dealer has not been assigned to this Deck"),
+    ("_remove_from_board", [OrderedDict()], DealerUnassignedError, "Dealer has not been assigned to this Deck"),
+    ("_add_to_board", [[]], DealerUnassignedError, "Dealer has not been assigned to this Deck"),
+    ("_burn_trash", [3], DealerUnassignedError, "Dealer has not been assigned to this Deck"),
+    ("_add_trash", [[]], DealerUnassignedError, "Dealer has not been assigned to this Deck"),
+    ("_take_from_players", [OrderedDict()], DealerUnassignedError, "Dealer has not been assigned to this Deck"),
+    ("_give_to_players", [[]], DealerUnassignedError, "Dealer has not been assigned to this Deck"),
+])
+def test_french_multiplayer_deck_manipulation_fails_dealer_unassigned(func_name, args, expected_exception, expected_message):
+    """
+    Functions that need Dealers assigned need ot fail and raise DealerUnassignedError
+
+    Test for
+    1) Make sure dealer is unassigned
+    2) Test each manipulation function and see that they raise DealerUnassignedError
+    """
+    deck = MultiPlayerDeck(name="French_Multi_Player_Deck_NoJoker", joker_count=0)
+
+    # 1) Dealer unassigned
+    assert deck.dealer_assigned == False
+
+    #2) Manipulation w/out dealers -> DealerUnassignedError
+    func = getattr(deck, func_name)
+    with pytest.raises(expected_exception, match=expected_message) as excinfo:
+        func(*args)
+    assert expected_message == str(excinfo.value)
 
 
-# if __name__ == "__main__":
-#     print("hi")
+def test_french_multiplayer_dealer_assignment_toggle_fails():
+    """Toggling deale assignment is not allowed outside of Game object"""
+    deck = MultiPlayerDeck(name="French_Multi_Player_Deck_NoJoker", joker_count=0)
+    # 1) Dealer unassigned
+    assert deck.dealer_assigned == False
+    deck.__dealer_assigned = True
+    assert deck.dealer_assigned == False
